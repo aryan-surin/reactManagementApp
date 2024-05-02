be3c588f0e5fe8aa3c67a2f5b4d50ec8f2ff9738
aryansurin <aryan.surin@augmentedscm.com>
2024-05-03 05:06:29 +0530

    worked with add and storing new project values.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,48 @@
+import { useState } from "react";
+
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import NewProject from "./components/NewProject.jsx";
 import ProjectsSidebar from "./components/ProjectsSidebar";
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId: undefined,
+    projects: [],
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => ({
+      ...prevState,
+      selectedProjectId: null,
+    }));
+  }
+
+  function handleAddProject(project) {
+    const newProject = {
+      ...project,
+      id: Math.random().toString(),
+    };
+
+    setProjectsState((prevState) => ({
+      ...prevState,
+      projects: [...prevState.projects, newProject],
+    }));
+    console.log(newProject);
+  }
+
+  let content;
+
+  if(projectsState.selectedProjectId === null) {
+    content = <NewProject onAdd={handleAddProject} />;
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NewProject />
+      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
