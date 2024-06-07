e4a0984e6c3426a222efd7b96b4e4ffc95db0f40
aryansurin <aryan.surin@augmentedscm.com>
2024-06-07 12:42:31 +0530

    add task and display title in side bar

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -12,6 +12,9 @@ export default function NewProject({ onAdd}) {
         const enteredDueDate = dueDate.current.value;
 
         // Validation
+        if (enteredTitle.trim().length === 0 || enteredDescription.trim().length === 0 || enteredDueDate.trim().length === 0) {
+            // Show the error modal.
+        }
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -23,7 +26,7 @@ export default function NewProject({ onAdd}) {
         <div className="w-[35rem] mt-16">
             <menu className="flex items-center justify-end gap-4 my-4">
                 <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancel</button>
+                    <button onClick={handleCancel} className="text-stone-800 hover:text-stone-950">Cancel</button>
                 </li>
                 <li>
                     <button onClick={handleSave} className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Save</button>
